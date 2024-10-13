81229b16e583bd8762cf94b817e3aceecf96bb77
Luiz Henrique F. Querobin <luizhfquerobin@gmail.com>
2024-10-13 14:31:38 -0300

    first commit

---
+++ src/main/java/io/github/luizhfquerobin/sbootexp_security/api/FooController.java
@@ -0,0 +1,21 @@
+package io.github.luizhfquerobin.sbootexp_security.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicRoute() {
+        return ResponseEntity.ok("Public route ok!");
+    }
+
+    @GetMapping("/private")
+    public ResponseEntity<String> privateRoute(Authentication authentication) {
+        System.out.println(authentication.getClass());
+        return ResponseEntity.ok("Private route ok! Usuário conectado: " + authentication.getName());
+    }
+}
---
+++ src/main/java/io/github/luizhfquerobin/sbootexp_security/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package io.github.luizhfquerobin.sbootexp_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider,
+                                                   CustomFilter customFilter) throws Exception {
+        return http
+                .authorizeHttpRequests(customizer -> {
+                    customizer.requestMatchers("/public").permitAll();
+                    customizer.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authenticationProvider(senhaMasterAuthenticationProvider)
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails commonUser = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(commonUser, adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
